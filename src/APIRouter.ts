bf19a52a5685357e6855b16d0938b89e028469cd
Kensaa <54020933+Kensaa@users.noreply.github.com>
2026-01-07 23:43:59 +0100

    fix: fix typo

--- src/APIRouter.ts
+++ src/APIRouter.ts
@@ -91,7 +91,7 @@ export type RouteHandler<
       /**
        *
        * @param req The request object containing the infos and data about the request
-       * @param res The response object. This object is not meant to send a response (yo ucan do that by returning an object matching the responseSchema), but to set cookies or any metadata like headers. If for some reason you really need to send the response yourself (by using the response object), the handler return value will be ignored
+       * @param res The response object. This object is not meant to send a response (you can do that by returning an object matching the responseSchema), but to set cookies or any metadata like headers. If for some reason you really need to send the response yourself (by using the response object), the handler return value will be ignored
        * @param instances The instances object passed to the router
        * @param userTokenData The data returned by the AuthHandler (the data contained in the token if you use the default jwt handler)
        * @returns The request response
